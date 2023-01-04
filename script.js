8f0702382ab527fe93ae869b6080d998e7909e70
gabrielss97 <gabriel.ssalvador97@gmail.com>
2023-01-04 16:28:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let liEls = document.querySelectorAll(".itens-carousel li");
+let index = 0;
+
+function show(increase) {
+  index = index + increase;
+  index = Math.min(Math.max(index, 0), liEls.length - 1);
+  liEls[index].scrollIntoView({ behavior: "smooth" });
+}
