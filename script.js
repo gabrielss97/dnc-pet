736f235474364d72f5e98c6fdd9f1ad4eab59729
Gabriel Soares Salvador <43815745+gabrielss97@users.noreply.github.com>
2023-01-05 18:39:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let liEls = document.querySelectorAll(".itens-carousel li");
-let index = 0;
 
-function show(increase) {
-  index = index + increase;
-  index = Math.min(Math.max(index, 0), liEls.length - 1);
-  liEls[index].scrollIntoView({ behavior: "smooth" });
-}
-
-setInterval(() => {
-  if (index === liEls.length - 1) {
-    show(index - 2);
-  } else {
-    show(index + 1);
-  }
-}, 5000);
